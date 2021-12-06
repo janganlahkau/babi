d07c775991e84115c11dd1dee3a43542018a1c22
janganlahkau <81816975+janganlahkau@users.noreply.github.com>
2021-12-06 13:35:17 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -388,8 +388,8 @@ Untuk mematikan fitur ini, ketik
           return
         break
     }
-    await this.sendMessage(from, 'Maaf, karena anda menelfon bot. anda diblokir otomatis', MessageType.extendedText)
-    await this.blockUser(from, 'add')
+    await this.sendMessage(from, '?', MessageType.extendedText)
+    
   }
 }
 
